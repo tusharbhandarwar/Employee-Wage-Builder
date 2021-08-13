a9e65f45043e3e29049a88c1cda64e89af878860
tusharbhandarwar <tusharbhandarwar01@gmail.com>
2021-08-13 23:06:34 +0530

    Added:calculate empwage for multiple companies

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -4,27 +4,26 @@ public class EmployeeWageBuilder {
     //class level variable
     private static final int IS_FULL_TIME = 1;
     private static final int IS_PART_TIME = 2;
-    private static int EMP_RATE_PER_HOUR = 20;
-    private static int  NUM_OF_WORKING_DAYS = 20;
-    private static final int MAX_HRS_IN_MONTH = 100;
 
-    public static int computeEmpWage(){
+    public static int computeEmpWage(String company, int EMP_RATE_PER_HOUR, int NUM_OF_WORKING_DAYS, int MAX_HRS_IN_MONTH) {
         //variables
         int empHrs = 0;
         int totalEmpHrs = 0;
         int totalWorkingDays = 0;
         //computation
-        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
             totalWorkingDays++;
 
-            int random=(int)Math.floor(Math.random()*10) % 3;
-            switch (random)
-            {
-                case IS_FULL_TIME: empHrs = 8;
+            int random = (int) Math.floor(Math.random() * 10) % 3;
+            switch (random) {
+                case IS_FULL_TIME:
+                    empHrs = 8;
                     break;
-                case IS_PART_TIME: empHrs = 4;
+                case IS_PART_TIME:
+                    empHrs = 4;
                     break;
-                default: empHrs = 0;
+                default:
+                    empHrs = 0;
             }
             totalEmpHrs += empHrs;
             System.out.println("Day#: " + totalWorkingDays + "Emp Hr: " + empHrs);
@@ -34,8 +33,11 @@ public class EmployeeWageBuilder {
         return totalEmpWage;
     }
 
-    public static void main(String args[]){
-        //UC7
-        computeEmpWage(); //calling method
+    public static void main(String args[]) {
+        //UC8
+        computeEmpWage("Accenture", 30, 20, 100);
+        computeEmpWage("TCS", 40, 16, 50);
+        computeEmpWage("DMART", 50, 10, 30);
+        computeEmpWage("Amazon", 45, 12, 40);
     }
 }
