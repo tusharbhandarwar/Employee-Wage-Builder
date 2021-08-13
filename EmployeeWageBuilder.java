e85d2590427992c7ce7eb08a6bd70990dff768cf
tusharbhandarwar <tusharbhandarwar01@gmail.com>
2021-08-13 23:10:06 +0530

    Added:total wage of each company

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,29 +1,43 @@
 package com.bridgelabz;
 
 public class EmployeeWageBuilder {
+    //UC9
     //class level variable
     private static final int IS_FULL_TIME = 1;
     private static final int IS_PART_TIME = 2;
 
-    public static int computeEmpWage(String company, int EMP_RATE_PER_HOUR, int NUM_OF_WORKING_DAYS, int MAX_HRS_IN_MONTH) {
+    private  final String company;
+    private final int EMP_RATE_PER_HOUR;
+    private final int NUM_OF_WORKING_DAYS;
+    private final int MAX_HRS_IN_MONTH;
+    private int totalEmpWage;
+
+
+    public EmployeeWageBuilder(String company, int EMP_RATE_PER_HOUR, int NUM_OF_WORKING_DAYS, int MAX_HRS_IN_MONTH){
+        this.company = company;
+        this.EMP_RATE_PER_HOUR = EMP_RATE_PER_HOUR;
+        this.NUM_OF_WORKING_DAYS = NUM_OF_WORKING_DAYS;
+        this.MAX_HRS_IN_MONTH = MAX_HRS_IN_MONTH;
+
+    }
+
+    public int computeEmpWage(){
         //variables
         int empHrs = 0;
         int totalEmpHrs = 0;
         int totalWorkingDays = 0;
         //computation
-        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
             totalWorkingDays++;
 
-            int random = (int) Math.floor(Math.random() * 10) % 3;
-            switch (random) {
-                case IS_FULL_TIME:
-                    empHrs = 8;
+            int random=(int)Math.floor(Math.random()*10) % 3;
+            switch (random)
+            {
+                case IS_FULL_TIME: empHrs = 8;
                     break;
-                case IS_PART_TIME:
-                    empHrs = 4;
+                case IS_PART_TIME: empHrs = 4;
                     break;
-                default:
-                    empHrs = 0;
+                default: empHrs = 0;
             }
             totalEmpHrs += empHrs;
             System.out.println("Day#: " + totalWorkingDays + "Emp Hr: " + empHrs);
@@ -33,11 +47,20 @@ public class EmployeeWageBuilder {
         return totalEmpWage;
     }
 
-    public static void main(String args[]) {
-        //UC8
-        computeEmpWage("Accenture", 30, 20, 100);
-        computeEmpWage("TCS", 40, 16, 50);
-        computeEmpWage("DMART", 50, 10, 30);
-        computeEmpWage("Amazon", 45, 12, 40);
+    public static void main(String args[]){
+
+        EmployeeWageBuilder accenture = new EmployeeWageBuilder("Accenture", 30, 20, 100);
+        EmployeeWageBuilder tcs = new EmployeeWageBuilder("TCS",40,16,50);
+        EmployeeWageBuilder dmart = new EmployeeWageBuilder("DMART", 50,10,30);
+        EmployeeWageBuilder amazon = new EmployeeWageBuilder("Amazon",30,12,40);
+        accenture.computeEmpWage();
+        System.out.println(accenture);
+        tcs.computeEmpWage();
+        System.out.println(tcs);
+        dmart.computeEmpWage();
+        System.out.println(dmart);
+        amazon.computeEmpWage();
+        System.out.println(amazon);
+
     }
 }
